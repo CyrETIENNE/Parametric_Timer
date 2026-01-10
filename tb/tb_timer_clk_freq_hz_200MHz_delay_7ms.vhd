5597f76db47634c4db61b37f200a2ec190d802f8
CyrETIENNE <cyril.etienne7.2001@gmail.com>
2026-01-10 10:57:34 +0100

    Add files via upload

---
+++ tb/tb_timer_clk_freq_hz_200MHz_delay_7ms.vhd
@@ -0,0 +1,53 @@
+library vunit_lib;
+context vunit_lib.vunit_context;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+use IEEE.math_real.ALL;
+
+entity tb_timer_clk_freq_hz_200MHz_delay_7ms is
+    generic (runner_cfg : string);
+end tb_timer_clk_freq_hz_200MHz_delay_7ms;
+
+architecture Behavioral of tb_timer_clk_freq_hz_200MHz_delay_7ms is
+
+    signal clk_i : std_ulogic := '0'; 
+    signal arst_i : std_ulogic := '0';
+    signal start_i : std_ulogic := '0'; 
+    signal done_o : std_ulogic;
+    constant clk_freq_hz_g : natural := 200e6; --200MHz
+    constant delay_g : time := 7 ms;
+    constant T_clock : time := (1e9/(2*clk_freq_hz_g))*1 ns;
+	constant my_checker : checker_t := new_checker("my_checker");
+
+begin
+
+    test_runner : process
+    begin
+    
+        test_runner_setup(runner, runner_cfg);
+        
+		wait until rising_edge(clk_i);
+		start_i <= '1';
+		wait until rising_edge(clk_i);
+		check(my_checker, done_o = '1', "Expected done_o to be logic 1 (not counting).");
+		wait until rising_edge(clk_i);
+		start_i <= '0';
+		wait until rising_edge(clk_i);
+		check(my_checker, done_o = '0', "Expected done_o to be logic 0 (counting).");	
+		wait for delay_g;
+		wait until rising_edge(clk_i);	
+		check(my_checker, done_o = '1', "Expected done_o to be logic 1 (not counting).");
+    
+        test_runner_cleanup(runner);
+        
+    end process;
+    
+    clk_i <= not clk_i after T_clock;
+    
+    timer : entity work.timer(Behavioral)
+        Generic map(clk_freq_hz_g => clk_freq_hz_g, delay_g => delay_g)
+        port map(clk_i => clk_i, arst_i => arst_i, start_i => start_i, done_o => done_o);
+
+
+end Behavioral;
